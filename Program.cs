1ed0dd2f7bc218bee18ea35d7eb84dad5edb35e5
InnokentySergeev <iaserg94@gmail.com>
2023-02-26 18:10:00 +0300

    final task program

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+string[] numbers = new string[6] { "1", "12", "123", "hello", "world", "2" };
+string[] finalnumbers = new string[6];
+
+void NumbersUnderFour(string[] numbers, string[] finalnumbers)
+{
+    int count = 0;
+
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        if (numbers[i].Length <= 3)
+        {
+            finalnumbers[i] = numbers[i];
+            count++;
+        }
+    }
+}
+
+void PrintArray(string[] finalnumbers)
+{
+    for (int i = 0; i < finalnumbers.Length; i++)
+    {
+        Console.Write($"{finalnumbers[i]} ");
+    }
+    Console.WriteLine();
+}
+
+NumbersUnderFour(numbers, finalnumbers);
+PrintArray(finalnumbers);
